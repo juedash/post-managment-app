d86addf03bee684efc50985102b171d819b83086
juedash <juedashherifi@hotmail.com>
2025-06-11 16:00:33 +0200

    remove redundant localstorage

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,4 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  localStorage.setItem('user', '1')
-})
 </script>
