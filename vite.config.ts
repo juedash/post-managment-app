f0b62c26a549085decc65104b857f060b4281b0a
juedash <juedashherifi@hotmail.com>
2026-01-30 11:28:11 +0100

    add gh-pages ready for deplyment

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+export default defineConfig(({command}) => ({
+  plugins: [vue(), tailwindcss(), ...(command === 'serve' ? [vueDevTools()] : [])],
+  base: '/post-managment-app/',
+
   server: {
     port: 3000,
     proxy: {
@@ -22,4 +24,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
